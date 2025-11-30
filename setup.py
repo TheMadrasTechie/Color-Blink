9061baa20010591cd9062de9ead26e0f2304aa15
TheMadrasTechie <techiemadras@gmail.com>
2025-11-30 10:07:56 +0530

    1-0-2-fixing-issues-in-code-snppets

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
 name='color_blink',
-version='1.0.1',
+version='1.0.2',
 packages=find_packages(),
 install_requires=[
     'opencv-python-headless==4.12.0.88',
@@ -15,12 +15,13 @@ install_requires=[
     'pillow==11.3.0'
 ],
 entry_points={
-"console_scripts": [
-"color_blink-reduce-image = color_blink:reduce_image_size_argparse",
-"color_blink-image-to-excel = color_blink:image_to_excel_argparse",
-"color_blink-blink-generate = color_blink:blink_excel_cell_argparse",
-"color_blink-test = color_blink:work",
-],},
+    "console_scripts": [
+        "color_blink-reduce-image = color_blink:reduce_image_size_argparse",
+        "color_blink-image-to-excel = color_blink:image_to_excel_argparse",
+        "color_blink-blink-generate = color_blink:blink_excel_cell_argparse",
+        "color_blink-test = color_blink:work",
+    ],
+},
 long_description=description,
 long_description_content_type="text/markdown",
 )
